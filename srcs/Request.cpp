1323b510a1a20888b325c1fd5424beb929810b02
EugenieF <eug.francon@gmail.com>
2022-09-21 22:59:45 +0200

    Test chunked request parsing on VM

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -203,6 +203,7 @@ int		Request::_parseChunks()
 	size_t			chunkSize;
 	std::string		chunk;
 	size_t			pos;
+	std::string		body;
 
 	if (_request.find("\r\n\r\n") == std::string::npos)
 		return (0);
@@ -214,8 +215,8 @@ int		Request::_parseChunks()
 		{
 			return (2);
 		}
-		std::cout << "---> chunkSizeStr : " << chunk << std::endl;
 		chunkSize = std::strtoul(chunk.c_str(), NULL, 16);
+		std::cout << "---> chunkSizeStr : '" << chunk << "' | size : " << chunkSize << std::endl;
 		if (chunk.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos || !chunkSize || chunkSize >= ULONG_MAX)
 		{
 			return (3);
@@ -227,7 +228,8 @@ int		Request::_parseChunks()
 		}
 		chunk = _getNextWord(chunkSize);
 		std::cout << "**********************************************" << std::endl;
-		std::cout << RED << "chunkSize : " << chunkSize << " | chunk = " << chunk << RESET << std::endl;
+		std::cout << RED << "chunkSize : " << chunkSize << " | chunk = '" << chunk << "'" << RESET << std::endl;
+		body += chunk;
 	}
 }
 
