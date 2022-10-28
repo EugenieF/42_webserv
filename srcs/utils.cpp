ce7023002dd7ff0de46c23b7222a14d2311deb29
EugenieF <eug.francon@gmail.com>
2022-10-28 20:57:57 +0200

    problem read binary

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -90,57 +90,57 @@ char* clone_str(const std::string& str) {
 	return (copy);
 }
 
-#include <sys/types.h>
-#include <sys/socket.h>
-# include <vector>
-
-std::string readFd(int fd) {
-	std::string     str;
-	ssize_t         count;
-
-	std::vector<char>	vecBuf(BUFSIZE, '\0');
-	std::vector<char>	vecStr;
-
-	count = read(fd, &vecBuf[0], vecBuf.size());
-	while (count) {
-		if (count < 0)
-			throw std::runtime_error("readFd (read) error");
-		vecStr.insert(vecStr.end(), vecBuf.begin(), vecBuf.end());
-		if (count == BUFSIZE) {
-			count = read(fd, &vecBuf[0], vecBuf.size());
-		} else {
-			break ;
-		}
-	}
-	for (std::vector<char>::iterator ite = vecStr.begin(); ite != vecStr.end(); ite++)
-	{
-		std::cout << YELLOW << *ite;
-		str += *ite;
-	}
-	std::cout << RESET << NL;
-	return (str);
-}
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// # include <vector>
 
 // std::string readFd(int fd) {
-// 	std::string     	str;
-// 	char            buf[BUFSIZE + 1];
+// 	std::string     str;
 // 	ssize_t         count;
 
-// 	memset(buf, 0, BUFSIZE + 1);
-// 	count = read(fd, buf, BUFSIZE);
+// 	std::vector<char>	vecBuf(BUFSIZE, '\0');
+// 	std::vector<char>	vecStr;
+
+// 	count = read(fd, &vecBuf[0], vecBuf.size());
 // 	while (count) {
 // 		if (count < 0)
 // 			throw std::runtime_error("readFd (read) error");
-// 		str += buf;
+// 		vecStr.insert(vecStr.end(), vecBuf.begin(), vecBuf.end());
 // 		if (count == BUFSIZE) {
-// 			memset(buf, 0, BUFSIZE + 1);
-// 			count = read(fd, buf, BUFSIZE);
+// 			count = read(fd, &vecBuf[0], vecBuf.size());
 // 		} else {
 // 			break ;
 // 		}
 // 	}
+// 	for (std::vector<char>::iterator ite = vecStr.begin(); ite != vecStr.end(); ite++)
+// 	{
+// 		std::cout << YELLOW << *ite;
+// 		str += *ite;
+// 	}
+// 	std::cout << RESET << NL;
 // 	return (str);
 // }
+
+std::string readFd(int fd) {
+	std::string     	str;
+	char            buf[BUFSIZE + 1];
+	ssize_t         count;
+
+	memset(buf, 0, BUFSIZE + 1);
+	count = read(fd, buf, BUFSIZE);
+	while (count) {
+		if (count < 0)
+			throw std::runtime_error("readFd (read) error");
+		str += buf;
+		if (count == BUFSIZE) {
+			memset(buf, 0, BUFSIZE + 1);
+			count = read(fd, buf, BUFSIZE);
+		} else {
+			break ;
+		}
+	}
+	return (str);
+}
 
 std::string		generateRandomString(size_t length)
 {
